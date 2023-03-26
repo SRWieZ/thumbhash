7548abfdaffd910d7e48b97af0971b4cf27c01ac
jerrywham <github@ecyseo.net>
2023-03-26 23:03:29 +0200

    Update ThumbhashTest.php

--- tests/Feature/ThumbhashTest.php
+++ tests/Feature/ThumbhashTest.php
@@ -1,16 +1,13 @@
 <?php
 
 use Thumbhash\Thumbhash;
-use function Thumbhash\extract_size_and_pixels_with_imagick;
+// use function Thumbhash\extract_size_and_pixels_with_imagick;
 
 test('it returns the correct hash', function ($url, $hash) {
 
-    $content = file_get_contents($url);
+    $Th = new Thumbhash($url);
 
-    list($width, $height, $pixels) = extract_size_and_pixels_with_imagick($content);
-
-    $encoded = Thumbhash::RGBAToHash($width, $height, $pixels);
-    $encodedBase64 = Thumbhash::convertHashToString($encoded);
+    $encodedBase64 = $Th->getHash();
 
     expect($encodedBase64)->toBe($hash);
 })
