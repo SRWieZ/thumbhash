13b50d485cc7c9052f17bff81f081e39a3c393e3
Riajul Islam <kmriajulislami@gmail.com>
2025-05-26 23:37:51 +0600

    Add `toAverageColor` method to extract average color from a ThumbHash (#17)
    * feat: Add method to convert ThumbHash to an average color hex string
    
    Introduce `toAverageColor` function to generate a hex color string in the `#AARRGGBB` format based on the average color of a ThumbHash. This complements the existing functionality by providing a more user-friendly color representation.
    
    * test: Add test for Thumbhash average color calculation
    
    Introduce a test to verify that Thumbhash correctly calculates the average color, using a new dataset `images_average_color`. The dataset includes various images with expected average color values, including those with transparency.

---
+++ tests/Datasets/ImagesAverageColor.php
@@ -0,0 +1,41 @@
+<?php
+dataset('images_average_color', [
+    [
+        'url'  => 'assets/sunrise.jpg',
+        'average_color' => '#FF565258',
+    ],
+    [
+        'url'  => 'assets/sunset.jpg',
+        'average_color' => '#FF6E7274',
+    ],
+    [
+        'url'  => 'assets/field.jpg',
+        'average_color' => '#FF6A7674',
+    ],
+    [
+        'url'  => 'assets/fall.jpg',
+        'average_color' => '#FF847759',
+    ],
+    [
+        'url'  => 'assets/street.jpg',
+        'average_color' => '#FF5F5B51',
+    ],
+    [
+        'url'  => 'assets/mountain.jpg',
+        'average_color' => '#FF626668',
+    ],
+    [
+        'url'  => 'assets/coast.jpg',
+        'average_color' => '#FF898583',
+    ],
+
+    // Images with transparency
+    [
+        'url'  => 'assets/firefox.png',
+        'average_color' => '#BBC2754E',
+    ],
+    [
+        'url'  => 'assets/opera.png',
+        'average_color' => '#77E51E2D',
+    ],
+]);
--- tests/Feature/ThumbhashTest.php
+++ tests/Feature/ThumbhashTest.php
@@ -15,3 +15,16 @@ test('it returns the correct hash', function ($url, $hash) {
     expect($encodedBase64)->toBe($hash);
 })
 ->with('images');
+
+test('it returns the correct average color', function ($url, $average_color) {
+
+    $content = file_get_contents($url);
+
+    list($width, $height, $pixels) = extract_size_and_pixels_with_imagick($content);
+
+    $encoded = Thumbhash::RGBAToHash($width, $height, $pixels);
+
+    $averageColor = (new Thumbhash)->toAverageColor($encoded);
+
+    expect($averageColor)->toBe($average_color);
+})->with('images_average_color');
